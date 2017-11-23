2bac0e89b114893bd09e42ff2022d3544c475add
Glitch (file-data-service) <none>
2017-11-23 12:56:21 +0000

    🌾🍭 Updated with Glitch

---
+++ public/client.js
@@ -0,0 +1,8 @@
+// client-side js
+// run by the browser each time your view template is loaded
+
+// by default, you've got jQuery,
+// add other scripts at the bottom of index.html
+$(document).ready(function(){
+  console.log("hello!")
+})
